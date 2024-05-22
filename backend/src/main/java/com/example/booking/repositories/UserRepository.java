403635ef4dded4c7650d6ac82b89bac5ecd7a3e2
vsiaurys <siaurys@gmail.com>
2024-05-22 21:38:29 +0300

    Modify method names in UserRepository (backend)

--- backend/src/main/java/com/example/booking/repositories/UserRepository.java
+++ backend/src/main/java/com/example/booking/repositories/UserRepository.java
@@ -6,9 +6,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findUserByEmail(String email);
+    Optional<User> findByEmail(String email);
 
-    Optional<User> findUserById(long id);
+    Optional<User> findById(long id);
 
     boolean existsByEmail(String email);
 }
