2a56df586af168638b61a3a68c69d6df5917947d
vsiaurys <siaurys@gmail.com>
2024-05-22 20:19:26 +0300

    Create UserRepository (backend)

---
+++ backend/src/main/java/com/example/booking/repositories/UserRepository.java
@@ -0,0 +1,14 @@
+package com.example.booking.repositories;
+
+import com.example.booking.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findUserByEmail(String email);
+
+    Optional<User> findUserById(long id);
+
+    boolean existsByEmail(String email);
+}
