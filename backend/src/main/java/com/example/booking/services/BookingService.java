2da1b94c019385e387832ffdf38a433c35cf33a7
vsiaurys <siaurys@gmail.com>
2024-05-22 22:02:14 +0300

    Create BookingService (backend)

---
+++ backend/src/main/java/com/example/booking/services/BookingService.java
@@ -0,0 +1,29 @@
+package com.example.booking.services;
+
+import com.example.booking.models.Booking;
+import com.example.booking.models.User;
+import com.example.booking.repositories.BookingRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Optional;
+
+public class BookingService {
+    private final BookingRepository bookingRepository;
+
+    @Autowired
+    public BookingService(BookingRepository bookingRepository) {
+        this.bookingRepository = bookingRepository;
+    }
+
+    public Booking saveBooking(Booking booking) {
+        return this.bookingRepository.save(booking);
+    }
+
+    public Optional<Booking> findBookingByUser(User user) {
+        return bookingRepository.findByUser(user);
+    }
+
+    public Optional<Booking> findBookingById(long id) {
+        return bookingRepository.findById(id);
+    }
+}
