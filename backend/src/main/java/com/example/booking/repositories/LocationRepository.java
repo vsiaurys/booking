b56eedb98444966beeffd3b1a981e5c1c01ff3e1
vsiaurys <siaurys@gmail.com>
2024-05-23 12:16:38 +0300

    Modify LocationRepository (backend)

--- backend/src/main/java/com/example/booking/repositories/LocationRepository.java
+++ backend/src/main/java/com/example/booking/repositories/LocationRepository.java
@@ -1,15 +1,14 @@
 package com.example.booking.repositories;
 
-import com.example.booking.models.Hotel;
 import com.example.booking.models.Location;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface LocationRepository extends JpaRepository<Location, Long> {
-    Optional<Hotel> findByName(String name);
+    Optional<Location> findByName(String name);
 
-    Optional<Hotel> findById(long id);
+    Optional<Location> findById(long id);
 
     boolean existsByName(String name);
 }
