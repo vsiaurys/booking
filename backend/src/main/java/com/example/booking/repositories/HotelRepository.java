f46af892ee93ae587d334ce987551e7dba31f02a
vsiaurys <siaurys@gmail.com>
2024-05-23 11:56:59 +0300

    Modify HotelRepository (backend)

--- backend/src/main/java/com/example/booking/repositories/HotelRepository.java
+++ backend/src/main/java/com/example/booking/repositories/HotelRepository.java
@@ -14,4 +14,6 @@ public interface HotelRepository extends JpaRepository<Hotel, Long> {
     Optional<Hotel> findById(long id);
 
     boolean existsByName(String name);
+
+    boolean existsByLocation(Location location);
 }
