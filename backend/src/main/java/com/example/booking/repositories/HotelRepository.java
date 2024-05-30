74c244d51fc2a95461f863f7e794940b3dbd3479
vsiaurys <siaurys@gmail.com>
2024-05-30 20:00:53 +0300

    Change method's findById return type in HotelRepository

--- backend/src/main/java/com/example/booking/repositories/HotelRepository.java
+++ backend/src/main/java/com/example/booking/repositories/HotelRepository.java
@@ -3,11 +3,10 @@ package com.example.booking.repositories;
 import com.example.booking.models.Hotel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface HotelRepository extends JpaRepository<Hotel, Long> {
-    List<Hotel> findByName(String name);
+    Optional<Hotel> findByName(String name);
 
     Optional<Hotel> findById(long id);
 
