d386797abb43bdb5f9dd3d6387f7c73b34692941
vsiaurys <siaurys@gmail.com>
2024-05-30 20:08:34 +0300

    Modify methods in HotelService

--- backend/src/main/java/com/example/booking/services/HotelService.java
+++ backend/src/main/java/com/example/booking/services/HotelService.java
@@ -1,12 +1,10 @@
 package com.example.booking.services;
 
 import com.example.booking.models.Hotel;
-import com.example.booking.models.Location;
 import com.example.booking.repositories.HotelRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -22,19 +20,15 @@ public class HotelService {
         return this.hotelRepository.save(hotel);
     }
 
-    public boolean existsHotelByLocation(String name, Location location) {
-        return hotelRepository.existsByLocation(location);
-    }
-
-    public Optional<Hotel> findHotelByLocation(Location location) {
-        return hotelRepository.findByLocation(location);
-    }
-
     public Optional<Hotel> findHotelById(long id) {
         return hotelRepository.findById(id);
     }
 
-    public List<Hotel> findAllHotelsByName(String name) {
+    public Optional<Hotel> findHotelByName(String name) {
         return hotelRepository.findByName(name);
+    }
+
+    public boolean existsHotelByName(String name) {
+        return hotelRepository.existsByName(name);
     }
 }
