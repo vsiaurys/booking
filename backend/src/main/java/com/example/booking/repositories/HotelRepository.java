912cffb9c068b918955edf68fd304c871370adef
vsiaurys <siaurys@gmail.com>
2024-05-30 19:47:39 +0300

    Delete unnecessary methods from HotelRepository

--- backend/src/main/java/com/example/booking/repositories/HotelRepository.java
+++ backend/src/main/java/com/example/booking/repositories/HotelRepository.java
@@ -1,7 +1,6 @@
 package com.example.booking.repositories;
 
 import com.example.booking.models.Hotel;
-import com.example.booking.models.Location;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -10,11 +9,7 @@ import java.util.Optional;
 public interface HotelRepository extends JpaRepository<Hotel, Long> {
     List<Hotel> findByName(String name);
 
-    Optional<Hotel> findByLocation(Location location);
-
     Optional<Hotel> findById(long id);
 
     boolean existsByName(String name);
-
-    boolean existsByLocation(Location location);
 }
