16f2e0214bfbbd49423e6a3d47c129c667095501
vsiaurys <siaurys@gmail.com>
2024-05-31 14:20:19 +0300

    Modify HotelLocationRepository

--- backend/src/main/java/com/example/booking/repositories/HotelLocationRepository.java
+++ backend/src/main/java/com/example/booking/repositories/HotelLocationRepository.java
@@ -1,5 +1,7 @@
 package com.example.booking.repositories;
 
 import com.example.booking.models.HotelLocation;
+import com.example.booking.models.HotelLocationId;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface HotelLocationRepository extends JpaRepository<HotelLocation, HotelLocationId> {}
