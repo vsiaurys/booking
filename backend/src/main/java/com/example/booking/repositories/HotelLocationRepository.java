789fed6d9586493c7c3264c5a91fb0c85d7b4d61
vsiaurys <siaurys@gmail.com>
2024-05-31 13:03:19 +0300

    Create HotelLocationRepository

---
+++ backend/src/main/java/com/example/booking/repositories/HotelLocationRepository.java
@@ -0,0 +1,5 @@
+package com.example.booking.repositories;
+
+import com.example.booking.models.HotelLocation;
+
+public interface HotelLocationRepository extends JpaRepository<HotelLocation, HotelLocationId> {}
