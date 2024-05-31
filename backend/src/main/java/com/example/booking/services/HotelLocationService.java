5eeb0a662fe663c285161313762c04d57a0b7d42
vsiaurys <siaurys@gmail.com>
2024-05-31 13:47:16 +0300

    Create HotelLocationService

---
+++ backend/src/main/java/com/example/booking/services/HotelLocationService.java
@@ -0,0 +1,38 @@
+package com.example.booking.services;
+
+import com.example.booking.models.HotelLocation;
+import com.example.booking.repositories.HotelLocationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class HotelLocationService {
+    private final HotelLocationRepository hotelLocationRepository;
+
+    @Autowired
+    public HotelLocationService(HotelLocationRepository hotelLocationRepository) {
+        this.hotelLocationRepository = hotelLocationRepository;
+    }
+
+    public List<HotelLocation> findAllHotelsLocations() {
+        return hotelLocationRepository.findAll();
+    }
+
+    public HotelLocation findHotelLocationById(HotelLocationId hotelLocationId) {
+        return hotelLocationRepository.findById(hotelLocationId).orElseThrow();
+    }
+
+    public HotelLocation saveHotelLocation(HotelLocation hotelLocation) {
+        return this.hotelLocationRepository.save(hotelLocation);
+    }
+
+    public boolean existsHotelLocationById(HotelLocationId hotelLocationId) {
+        return this.hotelLocationRepository.existsById(hotelLocationId);
+    }
+
+    public void deleteHotelLocationById(HotelLocationId hotelLocationId) {
+        this.hotelLocationRepository.deleteById(hotelLocationId);
+    }
+}
