f26ab311e36aee09438bd48b1429bd279de0bbf1
vsiaurys <siaurys@gmail.com>
2024-06-05 11:25:23 +0300

    Modify insertHotelLocation method in HotelLocationController

--- backend/src/main/java/com/example/booking/controllers/HotelLocationController.java
+++ backend/src/main/java/com/example/booking/controllers/HotelLocationController.java
@@ -65,8 +65,52 @@ public class HotelLocationController {
     }
 
     @PostMapping("/hotelslocations")
-    public HotelLocation insertHotelLocation(@RequestBody HotelLocation hotelLocation) {
-        return this.hotelLocationService.saveHotelLocation(hotelLocation);
+    public ResponseEntity<Object> insertHotelLocation(@RequestBody HotelLocation hotelLocation) {
+        Map<String, String> errors = new HashMap<>();
+
+        long hotelId = hotelLocation.getHotelLocationId().getHotel().getId();
+        long locationId = hotelLocation.getHotelLocationId().getLocation().getId();
+
+        Hotel hotel = this.hotelService.findHotelById(hotelId);
+        Location location = this.locationService.findLocationById(locationId);
+
+        if (hotel == null && location == null) {
+            errors.put("hotel", "Hotel with Id " + hotelId + " does not exist");
+            errors.put("location", "Location with Id " + locationId + " does not exist");
+
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
+        }
+
+        if (hotel == null) {
+            errors.put("hotel", "Hotel with Id " + hotelId + " does not exist");
+
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
+        }
+
+        if (location == null) {
+            errors.put("location", "Location with Id " + locationId + " does not exist");
+
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
+        }
+
+        String hotelName = hotelService.findHotelById(hotelId).getName();
+        String locationName = locationService.findLocationById(locationId).getName();
+
+        if (hotelLocationService.existsHotelLocationById(hotelLocation.getHotelLocationId())) {
+            errors.put("hotelLocationId", "Hotel " + hotelName + " in " + locationName + " already exists");
+
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
+        }
+
+        String hotelPicture = hotelService
+                .findHotelById(hotelLocation.getHotelLocationId().getHotel().getId())
+                .getPicture();
+
+        hotelLocation.getHotelLocationId().getHotel().setName(hotelName);
+        hotelLocation.getHotelLocationId().getHotel().setPicture(hotelPicture);
+        hotelLocation.getHotelLocationId().getLocation().setName(locationName);
+
+        return ResponseEntity.ok().body(this.hotelLocationService.saveHotelLocation(hotelLocation));
     }
 
     @GetMapping("/hotelslocations")
