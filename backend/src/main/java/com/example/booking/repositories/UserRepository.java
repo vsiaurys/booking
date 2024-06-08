cfc9368f2212354083f5ee65c288ce13b5c8042f
vsiaurys <siaurys@gmail.com>
2024-06-08 15:56:19 +0300

    Modify method findById in UserRepository

--- backend/src/main/java/com/example/booking/repositories/UserRepository.java
+++ backend/src/main/java/com/example/booking/repositories/UserRepository.java
@@ -8,7 +8,7 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
 
-    Optional<User> findById(long id);
+    User findById(long id);
 
     boolean existsByEmail(String email);
 }
