91ad40a2b47106da2a858161366aee414174f0af
vsiaurys <siaurys@gmail.com>
2024-06-08 15:51:16 +0300

    Create method findAllUsers in UserService

--- backend/src/main/java/com/example/booking/services/UserService.java
+++ backend/src/main/java/com/example/booking/services/UserService.java
@@ -5,6 +5,7 @@ import com.example.booking.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -30,5 +31,9 @@ public class UserService {
 
     public Optional<User> findUserById(long id) {
         return userRepository.findById(id);
+    }
+
+    public List<User> findAllUsers() {
+        return userRepository.findAll();
     }
 }
