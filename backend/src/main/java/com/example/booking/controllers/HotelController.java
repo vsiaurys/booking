3b50417949f7f87839173b303082ef78695d722b
vsiaurys <siaurys@gmail.com>
2024-06-23 23:35:52 +0300

    Update HOtelController

--- backend/src/main/java/com/example/booking/controllers/HotelController.java
+++ backend/src/main/java/com/example/booking/controllers/HotelController.java
@@ -2,6 +2,7 @@ package com.example.booking.controllers;
 
 import com.example.booking.models.Hotel;
 import com.example.booking.services.HotelService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,6 +16,7 @@ import java.util.Map;
 public class HotelController {
     private final HotelService hotelService;
 
+    @Autowired
     public HotelController(HotelService hotelService) {
         this.hotelService = hotelService;
     }
