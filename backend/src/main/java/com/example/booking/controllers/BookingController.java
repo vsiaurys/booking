90c99579a50feab9c5f700628d6289234a7d6c94
vsiaurys <siaurys@gmail.com>
2024-06-24 23:06:57 +0300

    Create method getBooking in BookingController

--- backend/src/main/java/com/example/booking/controllers/BookingController.java
+++ backend/src/main/java/com/example/booking/controllers/BookingController.java
@@ -9,10 +9,7 @@ import com.example.booking.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.List;
@@ -106,5 +103,19 @@ public class BookingController {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
         }
         return ResponseEntity.ok(listOfBookings);
+    }
+
+    @GetMapping("/bookings/{id}")
+    public ResponseEntity<Object> getBooking(@PathVariable long id) {
+        Booking booking = this.bookingService.findBookingById(id);
+
+        if (booking == null) {
+            Map<String, String> errors = new HashMap<>();
+            errors.put("booking", "Booking with Id " + id + " does not exist");
+
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
+        }
+
+        return ResponseEntity.ok(booking);
     }
 }
