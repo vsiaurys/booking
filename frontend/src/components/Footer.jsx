03f4410962e8fed1ffee167cf48186b80d2d427a
vsiaurys <siaurys@gmail.com>
2024-07-08 22:50:32 +0300

    Update component footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,3 +1,37 @@
+import "./Footer.css";
+
 export default function Footer() {
-  return <div className="d-flex flex-column min-vh-100">Footer</div>;
+  const getCurrentYear = () => {
+    const currentDate = new Date();
+    const currentYear = currentDate.getFullYear();
+
+    return currentYear;
+  };
+
+  return (
+    <div className="container text-start p-3 w-100">
+      <div className="row">
+        <div className="col-7 col-sm-6 col-md-8 col-lg-9">
+          <ul className="list-unstyled">
+            <li>&#169; {getCurrentYear()} HOTEL BOOKING PLATFORM</li>
+            <li>
+              <a
+                className="contact-email"
+                href="mailto:vardas.pavarde@gmail.com"
+              >
+                vardas.pavarde@gmail.com
+              </a>
+            </li>
+          </ul>
+        </div>
+
+        <div className="col-5 col-sm-6 col-md-4 col-lg-3 d-flex justify-content-end">
+          <ul className="list-unstyled">
+            <li>Trinapolio g. 2, Vilnius</li>
+            <li>+370 5 269 7455</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
