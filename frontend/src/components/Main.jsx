c92ef6b041b198080e0b9d8dcfda4d697f647683
vsiaurys <siaurys@gmail.com>
2024-07-17 23:16:30 +0300

    Update Main.jsx

--- frontend/src/components/Main.jsx
+++ frontend/src/components/Main.jsx
@@ -1,3 +1,7 @@
 export default function Main() {
-  return <div className="d-flex flex-column min-vh-100">Main</div>;
+  return (
+    <div className="d-flex flex-column min-vh-100">
+      A form for booking an hotel will appear here
+    </div>
+  );
 }
