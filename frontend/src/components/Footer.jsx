9d4e8115714189537fe9452b7744ce869c8a15f7
vsiaurys <siaurys@gmail.com>
2024-07-21 23:21:39 +0300

    Update Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -9,29 +9,46 @@ export default function Footer() {
   };
 
   return (
-    <div className="container text-start p-3 w-100">
-      <div className="row">
-        <div className="col-7 col-sm-6 col-md-8 col-lg-9">
-          <ul className="list-unstyled">
-            <li>&#169; {getCurrentYear()} HOTEL BOOKING PLATFORM</li>
-            <li>
-              <a
-                className="contact-email"
-                href="mailto:hotel.booking.platform@gmail.com"
-              >
-                hotel.booking.platform@gmail.com
-              </a>
-            </li>
-          </ul>
-        </div>
+    <footer className="mt-auto footer w-100">
+      <div className="container text-start p-3 w-100">
+        <div className="row">
+          <div className="col-sm-1 col-md-8 col-lg-9">
+            <ul className="list-unstyled">
+              <li>
+                <h5>Support</h5>
+              </li>
+              <li>Help center</li>
+              <li>Cancellation options</li>
+              <li>Manage your trips</li>
+              <li>Loyality program</li>
+            </ul>
+          </div>
 
-        <div className="col-5 col-sm-6 col-md-4 col-lg-3 d-flex justify-content-end">
-          <ul className="list-unstyled">
-            <li>Address in Vilnius</li>
-            <li>+370 5 123 4567</li>
-          </ul>
+          <div className="col-4 col-sm-6 col-md-4 col-lg-3 d-flex justify-content-end">
+            <ul className="list-unstyled">
+              <li>
+                <h5>Terms and settings</h5>
+              </li>
+              <li>Terms and conditions</li>
+              <li>Privacy & cookies</li>
+              <li>List your property</li>
+            </ul>
+          </div>
+
+          <div className="col-4 col-sm-6 col-md-4 col-lg-3 d-flex justify-content-end">
+            <ul className="list-unstyled">
+              <li>
+                <h5>About</h5>
+              </li>
+              <li>About HBP</li>
+              <li>New features</li>
+              <li>Careers</li>
+              <li>Investors</li>
+            </ul>
+          </div>
         </div>
+        <div>&#169; {getCurrentYear()} HOTEL BOOKING PLATFORM (HBP)</div>
       </div>
-    </div>
+    </footer>
   );
 }
