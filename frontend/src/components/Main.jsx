ca418b27bddc3eb61d1d059fdb021c21f9075138
vsiaurys <siaurys@gmail.com>
2024-07-23 23:36:01 +0300

    Add serach form to Main.jsx

--- frontend/src/components/Main.jsx
+++ frontend/src/components/Main.jsx
@@ -1,7 +1,36 @@
 export default function Main() {
   return (
     <div className="d-flex flex-column min-vh-100 bg-primary">
-      A form for booking an hotel will appear here
+      <h1 className="card-title display-6">Your next stay:</h1>
+      <form>
+        <div className="mb-3">
+          <input
+            type="text"
+            id="destination"
+            placeholder="Where to go?"
+          />
+        </div>
+        <div className="mb-3">
+          <input
+            type="text"
+            id="stay-dates"
+            placeholder="Check-in date -- Check-out date"
+          />
+        </div>
+        <div className="mb-3">
+          <input
+            type="text"
+            id="persons"
+            placeholder="2 adults, 0 chidren"
+          />
+        </div>
+        <button
+          type="submit"
+          className="btn button-search"
+        >
+          Search
+        </button>
+      </form>
     </div>
   );
 }
